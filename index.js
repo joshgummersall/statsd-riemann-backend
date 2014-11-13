8268b634005567d92c2ff2e62dce0e7b188d7f39
Josh Gummersall <gumby@moz.com>
2014-11-13 15:32:43 -0800

    Sketching out plugin

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 'use strict';
+
+var Riemann = require('./lib/riemann');
+
+exports.init = function(startupTime, config, events) {
+  var riemannInstance = new Riemann(startupTime, config, events);
+  return true;
+};
