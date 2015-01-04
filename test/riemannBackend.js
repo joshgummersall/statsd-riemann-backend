405eb153c7e799bb13254938d5177a5e1dab97f1
sam shiles <shilessam@hotmail.com>
2015-01-04 18:55:06 +0000

    Service name is now correctly stripped from the event string without the type and metic so that a service name is not changed by the value of the metic

--- test/riemannBackend.js
+++ test/riemannBackend.js
@@ -3,15 +3,33 @@ var config = { riemann: { host: {} } };
 var riemann = new require('../lib/riemann');
 
 describe("riemann-backend", function(){	    
+
 	var riemannBackend = new riemann({}, config, { on: function(){ } });
     
-    it("should parse a single metric to an array of one element", function(){
-    	var parsedMetrics = riemannBackend.parsePacket("foo:1|c\n");
-    	test.array(parsedMetrics).is(["foo:1|c"]);
-    });
+    describe("parsePacket", function() {
+	    it("should parse a single metric to an array of one element", function(){
+	    	var parsedMetrics = riemannBackend.parsePacket("foo:1|c\n");
+	    	test.array(parsedMetrics).is(["foo:1|c"]);
+	    });
 
-    it("should parse a two metrics to an array of two elements", function(){
-    	var parsedMetrics = riemannBackend.parsePacket("foo:1|c\nbar:2|c\n");
-    	test.array(parsedMetrics).is(["foo:1|c", "bar:2|c"]);
-    });
+	    it("should parse a two metrics to an array of two elements", function(){
+	    	var parsedMetrics = riemannBackend.parsePacket("foo:1|c\nbar:2|c\n");
+	    	test.array(parsedMetrics).is(["foo:1|c", "bar:2|c"]);
+	    });
+	});
+
+	describe("getService", function(){
+		it("should return the name without the metric and type", function(){
+			var serviceName = riemannBackend.getService("foo:1|c\n");
+			test.string(serviceName).is("foo");
+		});
+
+		it("should return the namespace without the metric and type, when parseNamespace is true", function(){
+			config.riemann.parseNamespace = true;
+			var serviceName = riemannBackend.getService("ns.foo:1|c\n");
+			config.riemann.parseNamespace = false;
+			test.string(serviceName).is("ns");
+		});
+	});
 }) 
+
